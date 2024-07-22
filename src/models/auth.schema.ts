7e55d3a768f2dcc2f1c40f9ebf8cff87c74ec084
David Olowomeye <olowomeyedavid242@gmail.com>
2024-07-22 15:39:33 +0100

    Added Database indexes

--- src/models/auth.schema.ts
+++ src/models/auth.schema.ts
@@ -1,64 +1,108 @@
 import { authSequelize } from '@auth/database';
 import { IAuthDocument } from '@greatdaveo/jobint-shared';
-import { DataTypes, ModelDefined, Optional } from 'sequelize';
+import { DataTypes, Model, ModelDefined, Optional } from 'sequelize';
+import { compare, hash } from 'bcryptjs';
+
+const salt = 10;
+
+// To correct the prototype error
+interface AuthModelInstanceMethods extends Model {
+  prototype: {
+    comparePassword: (password: string, hashedPassword: string) => Promise<boolean>;
+    hashPassword: (password: string) => Promise<string>;
+  };
+}
 
 type AuthUserCreationAttributes = Optional<IAuthDocument, 'id' | 'createdAt' | 'passwordResetToken' | 'passwordResetExpires'>;
 
-const AuthModel: ModelDefined<IAuthDocument, AuthUserCreationAttributes> = authSequelize.define('auths', {
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
+const AuthModel: ModelDefined<IAuthDocument, AuthUserCreationAttributes> & AuthModelInstanceMethods = authSequelize.define(
+  'auths',
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
 
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
 
-  profilePublicId: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
+    profilePublicId: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
 
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
 
-  country: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
 
-  profilePicture: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
+    profilePicture: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
 
-  emailVerificationToken: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
+    emailVerificationToken: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
 
-  emailVerified: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false,
-    defaultValue: 0
-  },
+    emailVerified: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: 0
+    },
 
-  createdAt: {
-    type: DataTypes.DATE,
-    defaultValue: Date.now
-  },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: Date.now
+    },
 
-  passwordResetToken: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
+    passwordResetToken: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
 
-  passwordResetExpires: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: new Date()
+    passwordResetExpires: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: new Date()
+    }
+  },
+  {
+    indexes: [
+      {
+        unique: true,
+        fields: ['email']
+      },
+      {
+        unique: true,
+        fields: ['username']
+      }
+    ]
   }
+) as ModelDefined<IAuthDocument, AuthUserCreationAttributes> & AuthModelInstanceMethods;
+
+// To Hash the password before it is save to the database
+AuthModel.addHook('beforeCreate', async (auth: Model) => {
+  const hashedPassword: string = await hash(auth.dataValues.password as string, 10);
+  auth.dataValues.password = hashedPassword;
 });
+
+// To compare the user password
+AuthModel.prototype.comparePassword = async function (password: string, hashedPassword: string): Promise<boolean> {
+  return compare(password, hashedPassword);
+};
+
+AuthModel.prototype.hashPassword = async function (password: string): Promise<string> {
+  return hash(password, 10);
+};
+
+AuthModel.sync({}); // To create the table if it doesn't exist
+export { AuthModel };
